c47237e1ccc9deaba2c66206ee0c1bfff9e5594d
Clayton Rieck <cjrieck123@gmail.com>
2014-08-22 09:05:51 -0400

    create initial app

---
+++ Sleek Play/Sleek PlayTests/Sleek_PlayTests.m
@@ -0,0 +1,34 @@
+//
+//  Sleek_PlayTests.m
+//  Sleek PlayTests
+//
+//  Created by Clayton Rieck on 8/22/14.
+//  Copyright (c) 2014 CLA. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Sleek_PlayTests : XCTestCase
+
+@end
+
+@implementation Sleek_PlayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
